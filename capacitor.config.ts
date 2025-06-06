65df5aac0eeaf6f3d44e30313ab49c3b6967405b
Moises56 <moiavil56@gmail.com>
2025-06-06 09:03:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.invoiceamdc',
+  appName: 'Invoice-amdc',
+  webDir: 'www'
+};
+
+export default config;
